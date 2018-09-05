6e74cd32591d932153d52739ceb90a09b9bd1bf3
Dan Pennebacker <dpennebacker@pvsd.org>
2018-09-05 14:43:04 -0400

    What do you mean? This is the version I did in class with Mr. P. Mr. P did the typing, I did the thinking.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int p1 = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+        int p2 = (179 + 21 + 10) / 7 + 181;
+        int p3 = 10389 * 56 * 11 + 2246;
+
+        System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 = " + p1);
+        System.out.println();
+        System.out.println("(179 + 21 + 10) / 7 + 181 = " + p2);
+        System.out.println();
+        System.out.println("10389 * 56 * 11 + 2246 = " + p3);
+
     }
 }
